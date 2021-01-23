57011f11bf9d5744590eeefcb7c433a03fb32981
Clivern <hello@clivern.com>
2021-01-23 22:57:24 +0100

    init

--- core/model/configs.go
+++ core/model/configs.go
@@ -5,60 +5,62 @@
 package model
 
 import (
+	"github.com/spacemanio/helmet/core/util"
+
 	yaml "gopkg.in/yaml.v2"
 )
 
 // Configs type
 type Configs struct {
-	App App `yaml:"app"`
+	App App `yaml:"app" json:"app"`
 }
 
 // App type
 type App struct {
-	Endpoint []Endpoint `yaml:"endpoint"`
+	Endpoint []Endpoint `yaml:"endpoint" json:"endpoint"`
 }
 
 // Endpoint type
 type Endpoint struct {
-	Name   string `yaml:"name"`
-	Active bool   `yaml:"active"`
-	Proxy  Proxy  `yaml:"proxy"`
+	Name   string `yaml:"name" json:"name"`
+	Active bool   `yaml:"active" json:"active"`
+	Proxy  Proxy  `yaml:"proxy" json:"proxy"`
 }
 
 // Proxy type
 type Proxy struct {
-	Upstreams      Upstreams      `yaml:"upstreams"`
-	HTTPMethods    []string       `yaml:"http_methods"`
-	Authentication Authentication `yaml:"authentication"`
-	RateLimit      RateLimit      `yaml:"rate_limit"`
-	CircuitBreaker CircuitBreaker `yaml:"circuit_breaker"`
-	ListenPath     string         `yaml:"listen_path"`
+	Upstreams      Upstreams      `yaml:"upstreams" json:"upstreams"`
+	HTTPMethods    []string       `yaml:"http_methods" json:"httpMethods"`
+	Authentication Authentication `yaml:"authentication" json:"authentication"`
+	RateLimit      RateLimit      `yaml:"rate_limit" json:"rateLimit"`
+	CircuitBreaker CircuitBreaker `yaml:"circuit_breaker" json:"circuitBreaker"`
+	ListenPath     string         `yaml:"listen_path" json:"listenPath"`
 }
 
 // Authentication type
 type Authentication struct {
-	Status bool `yaml:"status"`
+	Status bool `yaml:"status" json:"status"`
 }
 
 // Upstreams type
 type Upstreams struct {
-	Balancing string    `yaml:"balancing"`
-	Targets   []Targets `yaml:"targets"`
+	Balancing string    `yaml:"balancing" json:"balancing"`
+	Targets   []Targets `yaml:"targets" json:"targets"`
 }
 
 // Targets type
 type Targets struct {
-	Target string `yaml:"target"`
+	Target string `yaml:"target" json:"target"`
 }
 
 // RateLimit type
 type RateLimit struct {
-	Status bool `yaml:"status"`
+	Status bool `yaml:"status" json:"status"`
 }
 
 // CircuitBreaker type
 type CircuitBreaker struct {
-	Status bool `yaml:"status"`
+	Status bool `yaml:"status" json:"status"`
 }
 
 // LoadFromYAML update instance from YAML
@@ -70,4 +72,14 @@ func (c *Configs) LoadFromYAML(data []byte) error {
 	}
 
 	return nil
+}
+
+// LoadFromJSON update object from json
+func (c *Configs) LoadFromJSON(data []byte) error {
+	return util.LoadFromJSON(c, data)
+}
+
+// ConvertToJSON convert object to json
+func (c *Configs) ConvertToJSON() (string, error) {
+	return util.ConvertToJSON(c)
 }
