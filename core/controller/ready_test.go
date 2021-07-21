efee235485ca5b935ffbb944e6d37da8ca09300e
Clivern <hello@clivern.com>
2021-07-21 17:35:49 +0200

    test cases

---
+++ core/controller/ready_test.go
@@ -0,0 +1,51 @@
+// Copyright 2021 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package controller
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/spacewalkio/helmet/core/module"
+	"github.com/spacewalkio/helmet/pkg"
+
+	"github.com/franela/goblin"
+	"github.com/labstack/echo/v4"
+)
+
+// TestUnitReadyController
+func TestUnitReadyController(t *testing.T) {
+	g := goblin.Goblin(t)
+
+	pkg.LoadConfigs(fmt.Sprintf("%s/config.dist.yml", pkg.GetBaseDir("cache")))
+
+	database := &module.Database{}
+
+	// Reset DB
+	database.AutoConnect()
+	database.Rollback()
+	database.Migrate()
+
+	defer database.Close()
+
+	g.Describe("#Ready", func() {
+		g.It("It should satisfy all provided test cases", func() {
+			e := echo.New()
+			req := httptest.NewRequest(http.MethodGet, "/apigw/health", nil)
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetPath("/apigw/health")
+
+			err := Ready(c, &GlobalContext{Database: database})
+
+			g.Assert(err).Equal(nil)
+			g.Assert(rec.Code).Equal(http.StatusOK)
+			g.Assert(strings.TrimSpace(rec.Body.String())).Equal(`{"status":"i am ok"}`)
+		})
+	})
+}
