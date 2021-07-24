8e465bc57a68fb270bd7e9ec782ae9b4ee9fc2f1
Clivern <hello@clivern.com>
2021-07-24 14:26:41 +0200

    update

---
+++ core/component/correlation_test.go
@@ -0,0 +1,28 @@
+// Copyright 2021 Clivern. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
+
+package component
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/spacewalkio/helmet/pkg"
+
+	"github.com/franela/goblin"
+)
+
+// TestUnitCorrelation
+func TestUnitCorrelation(t *testing.T) {
+	baseDir := pkg.GetBaseDir("cache")
+	pkg.LoadConfigs(fmt.Sprintf("%s/config.dist.yml", baseDir))
+
+	g := goblin.Goblin(t)
+
+	g.Describe("#Correlation", func() {
+		g.It("It should satisfy test cases", func() {
+			g.Assert(NewCorrelation().UUIDv4() != "").Equal(true)
+		})
+	})
+}
