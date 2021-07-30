aff38564d26ff077ae9322767bce10aa9f9854f7
Clivern <hello@clivern.com>
2021-07-30 21:22:48 +0200

    fix & bump

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ WORKDIR /app
 
 COPY ./ ./
 
-RUN go build -v -ldflags="-X 'main.version=v1.0.15'" helmet.go
+RUN go build -v -ldflags="-X 'main.version=v1.0.16'" helmet.go
 
 FROM golang:1.16.6
 
--- core/controller/reverse_proxy.go
+++ core/controller/reverse_proxy.go
@@ -142,7 +142,7 @@ func ReverseProxy(c echo.Context, gc *GlobalContext) error {
 		name,
 		remote,
 		meta,
-		[]string{endpoint.Name, c.Request().Method, c.Request().URL.Path},
+		[]string{endpoint.Name, c.Request().Method, endpoint.Proxy.ListenPath},
 		c.Request().Header.Get("X-Request-ID"),
 	)
 
